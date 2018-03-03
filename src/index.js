815eec870471e5ebd7e8322ea7b0f5d6e63aca5c
PanFei <604042454@qq.com>
2018-03-03 09:33:01 +0800

    dev env

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import printMe from './print.js';
+
+function component() {
+    var element = document.createElement('div');
+    element.innerHTML = _.join(['Hello', 'webpack', ' ']);
+
+    element.classList.add('hello');
+
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+
+    var btn = document.createElement('button');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
+
+    return element;
+}
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+	entry: {
+		app: './src/index.js',
+		print: './src/print.js'
+	},
+	devtool: 'inline-source-map',
+	plugins: [
+		new CleanWebpackPlugin(['dist']),
+		new HtmlWebpackPlugin({
+			title: 'admin',
+			favicon: './icon.ico'
+		})
+	],
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: [
+				'style-loader',
+				'css-loader'
+			]
+		}, {
+			test: /\.(png|svg|jpg|gif)$/,
+			use: [
+				'file-loader'
+			]
+		}]
+	}
+};
